208e33e08e5a0f12f0c7652909ebef099df17e99
mkaidev <mkaidev88@gmail.com>
2024-08-04 21:26:14 +0700

    all products: upload image product

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import UploadProduct from "../components/UploadProduct";
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const [openUploadProduct, setOpenUploadProduct] = useState(false);
+
+  return (
+    <div>
+      <div className="bg-white py-2 px-4 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Products</h2>
+        <button
+          className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full"
+          onClick={() => setOpenUploadProduct(true)}
+        >
+          Upload Product
+        </button>
+      </div>
+
+      {/**upload prouct component */}
+      {openUploadProduct && (
+        <UploadProduct onClose={() => setOpenUploadProduct(false)} />
+      )}
+    </div>
+  );
 };
 
 export default AllProducts;
