4c792160f06c6119065beb3080640197b70026ba
mkaidev <mkaidev88@gmail.com>
2024-08-05 19:25:48 +0700

    fixed banner image

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
-import image1 from "../assets/banner/img1.webp";
-import image2 from "../assets/banner/img2.webp";
+import image1 from "../assets/banner/img1.jpg";
+import image2 from "../assets/banner/img2.jpg";
 import image3 from "../assets/banner/img3.jpg";
-import image4 from "../assets/banner/img4.jpg";
-import image5 from "../assets/banner/img5.webp";
 
 import image1Mobile from "../assets/banner/img1_mobile.jpg";
-import image2Mobile from "../assets/banner/img2_mobile.webp";
+import image2Mobile from "../assets/banner/img2_mobile.jpg";
 import image3Mobile from "../assets/banner/img3_mobile.jpg";
-import image4Mobile from "../assets/banner/img4_mobile.jpg";
-import image5Mobile from "../assets/banner/img5_mobile.png";
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
@@ -17,15 +13,9 @@ import { FaAngleLeft } from "react-icons/fa6";
 const BannerProduct = () => {
   const [currentImage, setCurrentImage] = useState(0);
 
-  const desktopImages = [image1, image2, image3, image4, image5];
+  const desktopImages = [image1, image2, image3];
 
-  const mobileImages = [
-    image1Mobile,
-    image2Mobile,
-    image3Mobile,
-    image4Mobile,
-    image5Mobile,
-  ];
+  const mobileImages = [image1Mobile, image2Mobile, image3Mobile];
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const nextImage = () => {
@@ -81,7 +71,7 @@ const BannerProduct = () => {
                 key={imageURl}
                 style={{ transform: `translateX(-${currentImage * 100}%)` }}
               >
-                <img src={imageURl} className="w-full h-full" alt="" />
+                <img src={imageURl} className="w-full h-full" alt="banner" />
               </div>
             );
           })}
@@ -98,8 +88,8 @@ const BannerProduct = () => {
               >
                 <img
                   src={imageURl}
-                  className="w-full h-full object-cover"
-                  alt=""
+                  className="w-full h-full object-fill"
+                  alt="banner"
                 />
               </div>
             );
--- frontend/src/components/HorizontalCardProduct.js
+++ frontend/src/components/HorizontalCardProduct.js
@@ -69,7 +69,7 @@ const HorizontalCardProduct = ({ category, heading }) => {
                   <div className="bg-slate-200 h-full p-4 min-w-[120px] md:min-w-[145px] animate-pulse"></div>
                   <div className="p-4 grid w-full gap-2">
                     <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black bg-slate-200 animate-pulse p-1 rounded-full">
-                      🛒
+                      🛒Loading...
                     </h2>
                     <p className="capitalize text-slate-500 p-1 bg-slate-200 animate-pulse rounded-full"></p>
                     <div className="flex gap-3 w-full">
